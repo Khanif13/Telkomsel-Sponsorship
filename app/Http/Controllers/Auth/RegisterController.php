f0a352cbc74439e530c4624d0d3a5d93cb95e029
khanif13 <ahmadhanif13125@gmail.com>
2026-02-24 10:00:42 +0800

    Update: update Validator & Create method, match Telkomsel number

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -43,7 +42,6 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -52,21 +50,49 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'phone_number' => [
+                'required',
+                'string',
+                'min:10',
+                'max:15',
+                function ($attribute, $value, $fail) {
+                    if (preg_match('/^(?:0|\+?62)(\d{3})/', $value, $matches)) {
+                        $prefix = $matches[1];
+                        $validPrefixes = ['811', '812', '813', '821', '822', '823', '851', '852', '853'];
+                        if (! in_array($prefix, $validPrefixes)) {
+                            $fail("{$prefix} bukan prefix nomor Telkomsel yang valid.");
+                        }
+                    } else {
+                        $fail('Format nomor telepon tidak valid.');
+                    }
+                },
+            ],
         ]);
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
      * @return \App\Models\User
      */
     protected function create(array $data)
     {
+        $baseUsername = explode('@', $data['email'])[0];
+        $username = $baseUsername;
+        $counter = 1;
+
+        while (\App\Models\User::where('username', $username)->exists()) {
+            $username = $baseUsername.$counter;
+            $counter++;
+        }
+
         return User::create([
             'name' => $data['name'],
+            'username' => $username,
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'phone_number' => $data['phone_number'],
+            'password' => \Illuminate\Support\Facades\Hash::make($data['password']),
+            'role' => 'user',
         ]);
     }
 }
